33d77d0c006bb4901182e72650a640c4468738a9
dileepvr <dileepvr@gmail.com>
2016-01-16 17:27:50 -0800

    Server sends RGBXXX, where the three Xs' are single byte color values for RGB. It sends it after a name has been assigned.

--- main.cpp
+++ main.cpp
@@ -584,6 +584,12 @@ int main(int argc, char* argv[]) {
 	    
 	      if(myTad[i].TCP_limbo) {
 		myTad[i].spawn(buf, i);
+		buf[0] = 'R'; buf[1] = 'G'; buf[2] = 'B';
+		buf[5] = tadcolor[i] & 0xFF;
+		buf[4] = (tadcolor[i] >> 8) & 0xFF;
+		buf[3] = (tadcolor[i] >> 16) & 0xFF;
+		buf[6] = '\0';
+		SDLNet_TCP_Send(myTad[i].socket,(void*)buf,7);
 	      } else {
 		for(j = 0; j < receivedByteCount; j++) {
 		  // If client asked to quit by sending 'K' as first char
